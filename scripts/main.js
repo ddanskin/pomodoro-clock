caf7b8439b0e3a5f37dd5c49bad67690ee72d9e9
Danielle Danskin <danielle.danskin@gmail.com>
2018-04-15 13:13:34 -0400

    fix spacing and variable/element names

--- scripts/main.js
+++ scripts/main.js
@@ -1,44 +1,56 @@
-let first_session = true;
+let firstSession = true;
 let workTime = true;
-let clock_paused = true;
+let clockPaused = true;
 let bTime = 5;
 let wTime = 25;
 let t = wTime;
 let minutes = t;
 let seconds = 0;
 let counter;
-let sound_file = new Audio("http://soundbible.com/mp3/Spit_Splat-Mike_Koenig-1170500447.mp3");
+let soundFile = new Audio("http://soundbible.com/mp3/Spit_Splat-Mike_Koenig-1170500447.mp3");
+
 reset();
-function setTimer(btn){
+
+function setTimer(btn) {
     let b = btn;
     
-    if(b == "bP"){
+    if (b == "bP"){
         bTime++;
-        if(bTime == 2) {
+
+        if (bTime == 2) {
             document.getElementById("bM").disabled = false;
         }
+
         document.getElementById("breakT").innerHTML = bTime;
-    } else if(b == "wP"){
+   
+    } else if (b == "wP") {
         wTime++;
-        if(wTime == 2) {
+        
+        if (wTime == 2) {
             document.getElementById("wM").disabled = false;
         }
+        
         document.getElementById("workT").innerHTML = wTime;
-    } else if(b == "bM") {
+   
+    } else if (b == "bM") {
         bTime--;
-        if(bTime == 1) {
+    
+        if (bTime == 1) {
             document.getElementById("bM").disabled = true;
         }
+        
         document.getElementById("breakT").innerHTML = bTime;
-    } else if(b == "wM") {
+    
+    } else if (b == "wM") {
         wTime--;
-        if(wTime == 1) {
+    
+        if (wTime == 1) {
             document.getElementById("wM").disabled = true;
         }
         document.getElementById("workT").innerHTML = wTime;
     }
     
-    if(clock_paused && workTime){
+    if (clockPaused && workTime) {
         t = wTime;
         minutes = t;
         seconds = 0;
@@ -46,29 +58,38 @@ function setTimer(btn){
     }
 }
 
+let setterElements = document.getElementsByClassName("adjustBreak, adjustWork");
+Array.from(setterElements, setter => setter.addEventListener('click', function(){
+    setTimer(setter.id);
+});
+
 document.getElementById("countdown").addEventListener("click", function() {
-    if (clock_paused && first_session){
+    
+    if (clockPaused && firstSession) {
         document.getElementById("pause").innerHTML = "||";
-        startTimer(t*60);
-        first_session = false;
-    } else if (clock_paused) {
+        startTimer(t * 60);
+        firstSession = false;
+    
+    } else if (clockPaused) {
         document.getElementById("pause").innerHTML = "||";
         startTimer(counter);
+    
     } else {
         document.getElementById("pause").innerHTML = ">";
+    
     }
 
-    clock_paused = !clock_paused;
+    clockPaused = !clockPaused;
 });
 
 document.getElementById("reset").addEventListener("click", function() {
     reset(wTime);
 });
 
-function reset(){
+function reset() {
     workTime = true;
-    clock_paused = true;
-    first_session = true;
+    clockPaused = true;
+    firstSession = true;
     wTime = 25;
     bTime = 5;
     t = wTime;
@@ -77,21 +98,21 @@ function reset(){
     updateDisplay();
 }
 
-function startTimer(x){
+function startTimer(x) {
     counter = x;
     updateDisplay();
     let interval = setInterval(() => {
     
-        if(clock_paused) {
+        if (clockPaused) {
             clearInterval(interval);
         } else {
             counter--;
-            minutes = Math.floor(counter/60);
+            minutes = Math.floor(counter / 60);
             seconds = Math.floor(counter % 60);
             document.getElementById("timer").innerHTML = addZero(minutes, 2) + ":" + addZero(seconds, 2);
             
             if (counter == 0) {
-                sound_file.play();
+                soundFile.play();
                 
                 if (workTime) {
                     t = bTime;
@@ -101,7 +122,7 @@ function startTimer(x){
                 document.getElementById("timer").innerHTML = addZero(minutes, 2) + ":" + addZero(seconds, 2);
                 workTime = !workTime;
                 clearInterval(interval);
-                startTimer(t*60);
+                startTimer(t * 60);
             }
         }
     }, 1000);
@@ -117,7 +138,7 @@ function updateDisplay() {
     document.getElementById("timer").innerHTML = addZero(minutes, 2) + ":" + addZero(seconds, 2);
 }
 
-function addZero(x, n){
+function addZero(x, n) {
     while (x.toString().length < n) {
         x = "0" + x;
     }
